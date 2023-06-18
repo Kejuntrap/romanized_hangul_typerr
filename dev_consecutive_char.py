a1c8a25dbe44749899474c30d94f26c8b877b250
Kejun <kfktit7@gmail.com>
2023-06-18 15:01:40 +0900

    :zap: 連続する文字を入力するときの入力についての改善開始

---
+++ dev_consecutive_char.py
@@ -0,0 +1,15 @@
+import io, sys
+import jk_converter as jk
+import romanized_hangul_typerr as rh
+
+#sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+#sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
+# 現在の入力法では　seokyeol（석열）を連続して打てないのでそれの改善を行う
+
+def main():
+    print("あ")
+
+
+if __name__ == '__main__':
+    main()
